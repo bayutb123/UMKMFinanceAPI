8c2563e93c0f21e09ccb3355abf2c86a432737fa
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2023-10-13 20:53:56 +0700

    add code 500 error response at login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,11 +9,9 @@ use App\Http\Requests\LoginRequest;
 
 class AuthController extends Controller
 {
-
     public function login(LoginRequest $request)
     {
         $validated = $request->validated();
-
         if (!auth()->attempt($validated)) {
             return response()->json([
                 'api_status' => '401',
@@ -22,14 +20,20 @@ class AuthController extends Controller
         }
 
         $user = User::where('email', $request->email)->first();
-        $token = $user->createToken('auth_token')->plainTextToken;
+        if ($user) {
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json([
+                'api_status' => '200',
+                'message' => 'Authorized',
+                'user' => $user,
+                'token' => $token,
+            ], 200);
+        }
 
         return response()->json([
-            'api_status' => '200',
-            'message' => 'Authorized',
-            'user' => $user,
-            'token' => $token,
-        ], 200);
+            'api_status' => '500',
+            'message' => 'Internal Server Error',
+        ], 500);
     }
 
     public function register(RegisterRequest $request)
